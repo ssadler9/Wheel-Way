2efc286aad08c4b176807064cf1b3a1682e974d7
Sam Sadler <samsadler9@Sams-MacBook-Air.local>
2017-09-16 13:23:56 -0500

    adding firebase

---
+++ assets/java.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCjr7ERSIfJac4JLYwoRPnxlsgawaArVCs",
+    authDomain: "project-1-dd3d4.firebaseapp.com",
+    databaseURL: "https://project-1-dd3d4.firebaseio.com",
+    projectId: "project-1-dd3d4",
+    storageBucket: "project-1-dd3d4.appspot.com",
+    messagingSenderId: "747176525785"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();

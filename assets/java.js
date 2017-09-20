c29cea922bde3d8e4216e92520d27c5998df03ac
Sofia <sz14g@my.fsu.edu>
2017-09-20 18:48:13 -0500

    weather api

--- assets/java.js
+++ assets/java.js
@@ -1,3 +1,5 @@
+$( document ).ready(function() {
+
 // Initialize Firebase
   var config = {
     apiKey: "AIzaSyCjr7ERSIfJac4JLYwoRPnxlsgawaArVCs",
@@ -10,3 +12,38 @@
   firebase.initializeApp(config);
 
 var database = firebase.database();
+
+// Weather API/AJAX call
+var weatherAPIKey = "a910455ef73b594f1148b29789a79ba8";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?" +
+      "q=austin,us&units=imperial&appid=" + weatherAPIKey;
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+.done(function(response) {
+  console.log(queryURL);
+  console.log(response);
+
+  $("#city").html(response.name);
+  $("#description").html(response.weather[0].description);
+  $("#weather_img").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+  $("#temp").prepend(response.main.temp);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

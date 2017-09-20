173194cab420d3f154acaa5986923a44f558afaf
Sam Sadler <samsadler9@Sams-MacBook-Air.local>
2017-09-20 12:49:24 -0500

    adding navigation

--- assets/java.js
+++ assets/java.js
@@ -8,18 +8,123 @@
     messagingSenderId: "747176525785"
   };
   firebase.initializeApp(config);
+// storing firebase inside variable
+var database = firebase.database();
+console.log(database);
+
+// variables for data in firebase
+  
+
+
+// labels and labelindex set for pins on google mpas
+      var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      var labelIndex = 0;
+// functoin that displays the map
+    function initMap() {
+
+      // Copied from Google to make navigation apper
+      // ===
+      var markerArray = [];
+      // Instantiate a directions service.
+      var directionsService = new google.maps.DirectionsService;
+      // ===
+
+      var bangalore = { lat: 30.286235, lng: -97.739396 };
+         map = new google.maps.Map(document.getElementById('gmap'), {
+          zoom: 15,
+          center: bangalore
+        });
+
+//copied from google for navigation
+         // ===
+         // Create a renderer for directions and bind it to the map.
+        var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+        // Instantiate an info window to hold step text.
+        var stepDisplay = new google.maps.InfoWindow;
+        // Display the route between the initial start and end selections.
+        calculateAndDisplayRoute(
+            directionsDisplay, directionsService, markerArray, stepDisplay, map);
+          var onChangeHandler = function() {
+          calculateAndDisplayRoute(
+              directionsDisplay, directionsService, markerArray, stepDisplay, map);
+        };
+         // ===
+
+        // This event listener calls addMarker() when the map is clicked.
+        google.maps.event.addListener(map, 'click', function(event) {
+          addMarker(event.latLng, map);
+        });
+        // Add a marker at the center of the map.
+        addMarker(bangalore, map);
+
+        // copied from google for navigation
+        document.getElementById('start').addEventListener('change', onChangeHandler);
+        document.getElementById('end').addEventListener('change', onChangeHandler);
+      }
+      // Adds a marker to the map.
+      function addMarker(location, map) {
+        // Add the marker at the clicked location, and add the next-available label
+        // from the array of alphabetical characters.
+        var marker = new google.maps.Marker({
+          position: location,
+          label: labels[labelIndex++ % labels.length],
+          map: map
+          });
+      }  
+// rest of code from google for navigation
+      function calculateAndDisplayRoute(directionsDisplay, directionsService,
+          markerArray, stepDisplay, map) {
+        // First, remove any existing markers from the map.
+        for (var i = 0; i < markerArray.length; i++) {
+          markerArray[i].setMap(null);
+        }
+
+        // Retrieve the start and end locations and create a DirectionsRequest using
+        // WALKING directions.
+        directionsService.route({
+          origin: document.getElementById('start').value,
+          destination: document.getElementById('end').value,
+          travelMode: 'WALKING'
+        }, function(response, status) {
+          // Route the directions and pass the response to a function to create
+          // markers for each step.
+          if (status === 'OK') {
+            document.getElementById('warnings-panel').innerHTML =
+                '<b>' + response.routes[0].warnings + '</b>';
+            directionsDisplay.setDirections(response);
+            showSteps(response, markerArray, stepDisplay, map);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
+
+      function showSteps(directionResult, markerArray, stepDisplay, map) {
+        // For each step, place a marker, and add the text to the marker's infowindow.
+        // Also attach the marker to an array so we can keep track of it and remove it
+        // when calculating new routes.
+        var myRoute = directionResult.routes[0].legs[0];
+        for (var i = 0; i < myRoute.steps.length; i++) {
+          var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+          marker.setMap(map);
+          marker.setPosition(myRoute.steps[i].start_location);
+          attachInstructionText(
+              stepDisplay, marker, myRoute.steps[i].instructions, map);
+        }
+      }
+
+      function attachInstructionText(stepDisplay, marker, text, map) {
+        google.maps.event.addListener(marker, 'click', function() {
+          // Open an info window when the marker is clicked on, containing the text
+          // of the step.
+          stepDisplay.setContent(text);
+          stepDisplay.open(map, marker);
+        });
+      }
+         
+        
+    
 
 
 
 
-var update_timeout = null;
-
-google.maps.event.addListener(map, 'click', function(event){
-    update_timeout = setTimeout(function(){
-        do_something_here();
-    }, 200);        
-});
-
-google.maps.event.addListener(map, 'dblclick', function(event) {       
-    clearTimeout(update_timeout);
-});
